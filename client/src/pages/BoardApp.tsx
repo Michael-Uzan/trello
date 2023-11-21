14861c985bc5f93b7f3eb1319d4a3ca8dfb2ca91
Michael Uzan <looply3@gmail.com>
2023-11-21 23:55:14 +0200

    working on board app

--- client/src/pages/BoardApp.tsx
+++ client/src/pages/BoardApp.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { boardService } from '../services/board.service';
+import { useParams } from 'react-router-dom';
+import { IBoard } from '../interfaces/IBoard';
 
 export const BoardApp = () => {
-    return (
-        <div>
+  const { boardId } = useParams();
+  const [board, setBoard] = useState<IBoard | null>(null);
+  const [notFound, setNotFound] = useState<boolean>(false);
 
-        </div>
-    )
-}
+  useEffect(() => {
+    const loadBoard = async (boardId: string) => {
+      console.log('boardId: ', boardId);
+      // setLoading(true);
+      try {
+        const board: IBoard = await boardService.getBoardById(boardId);
+        console.log('board: ', board);
+        if (board) {
+          setBoard(board);
+        } else {
+          setNotFound(true);
+        }
+      } catch (err) {
+        // setError(true);
+      }
+    };
+
+    if (boardId) {
+      loadBoard(boardId);
+    }
+  }, [boardId]);
+
+  if (notFound) {
+    return <div>{'Not Found'}</div>;
+  }
+
+  if (!board) {
+    return <div>{'loading'}</div>;
+  }
+
+  return <div>{board.title}</div>;
+};
